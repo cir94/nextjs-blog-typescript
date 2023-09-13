44dc31d0767a8c40478ae00ef2c4c130ca1fe7b8
cir94 <claire.isabella.ramos@gmail.com>
2023-09-13 15:24:47 -0400

    added styles folder with carried over CSS, applied to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import utilStyles from '../../styles/utils.module.css'
+
 export default function Home() {
   return (
     <main>
-      <section>
+      <section className={`${utilStyles.headingMd}`}>
         <p>Hi, I'm Claire, an aspiring software developer with a foundation in IT.</p>
         <p>This little blog was originally a Blog Tutorial provided by NextJS to help teach the user its syntax. However, it wasn't written in TypeScript. Therefore, I'll be trying to rewrite this entire project in NextJS, while also maybe adding some different features. It'll be my own little playground.</p>
       </section>
-      <section>
-        <h2>Blog</h2>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
       </section>
     </main>
   )
